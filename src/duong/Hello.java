7ed8c5ec8e4068616733a5ed03a195d89452c64a
duongnot <nongvanduongdv@gmail.com>
2024-06-28 08:09:54 +0700

    init project

---
+++ src/duong/Hello.java
@@ -0,0 +1,10 @@
+package duong;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello world wiht java");
+		System.out.println("Hello world wiht java");
+		System.out.println("Hello world wiht java");
+		System.out.println("Hello world wiht java");
+	}
+}
